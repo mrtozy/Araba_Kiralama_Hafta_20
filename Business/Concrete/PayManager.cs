d7974aa391cd9a789078f88fa0389d9525b0586a
m-ozs <m-ozs@MRTOZY>
2023-01-17 17:07:13 +0300

    Add project files.

---
+++ Business/Concrete/PayManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PaymentManager : IPayService
+    {
+        IPayDal _paymentDal;
+
+        public PaymentManager(IPayDal paymentDal)
+        {
+            _paymentDal = paymentDal;
+        }
+
+        [ValidationAspect(typeof(PaymentValidator))]
+        public IResult Pay(Payment payment)
+        {
+            _paymentDal.Add(payment);
+            return new SuccessResult( Messages.ArabaKiralandi);
+        }
+
+        public IResult Add(Payment payment)
+        {
+            _paymentDal.Add(payment);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Payment payment)
+        {
+            _paymentDal.Delete(payment);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Payment>> GetAll()
+        {
+            return new SuccessDataResult<List<Payment>>(_paymentDal.GetAll());
+        }
+
+        public IResult Update(Payment payment)
+        {
+            _paymentDal.Update(payment);
+            return new SuccessResult();
+        }
+    }
+}
